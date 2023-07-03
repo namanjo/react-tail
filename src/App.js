f9ef68eace476b91eda6af7535686d6c9904b36a
Naman Joshi <naman14joshi@gmail.com>
2023-07-04 00:25:31 +0530

    feat: added newsletter section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
+import Newsletter from "./components/Newsletter";
 
 
 
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Hero />
       <Analytics />
+      <Newsletter />
     </div>
   );
 }
