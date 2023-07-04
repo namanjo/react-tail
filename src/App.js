ee61ce062da4e9c67f1fb1594f9b4f9840709cf3
Naman Joshi <naman14joshi@gmail.com>
2023-07-04 16:56:15 +0530

    feat: created footer section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Hero from "./components/Hero";
 import Analytics from "./components/Analytics";
 import Newsletter from "./components/Newsletter";
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <Analytics />
       <Newsletter />
       <Cards />
+      <Footer />
     </div>
   );
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
             '>Grow with data.</h1>
             <div className='flex justify-center items-center'>
                 <p className='md:text-5xl sm:text-4xl text-xl font-bold pb-4'>Fast, flexible financing for</p>
-                <Typed className='md:text-5xl sm:text-4xl text-xl font-bold ml-2' strings={['BTB', 'BTC', 'SAAS']} typeSpeed={120} backSpeed={140} loop />
+                <Typed className='md:text-5xl sm:text-4xl text-xl font-bold ml-2 mb-4' strings={['BTB', 'BTC', 'SAAS']} typeSpeed={120} backSpeed={140} loop />
             </div>
             <p className='md:text-2xl text-xl font-bold text-gray-500'>Monitor your data analytics to increase revenue for BTB, BTC, & SAAS platforms</p>
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Get Started</button>
